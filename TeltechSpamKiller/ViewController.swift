0a23dbd930506ec195176310eef3da3ac3b1274e
Danijel Trifunović <danijelt055@gmail.com>
2023-09-28 19:25:29 +0200

    Initial Commit

---
+++ TeltechSpamKiller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeltechSpamKiller
+//
+//  Created by DTech on 28.09.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
